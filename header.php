498ac8aec149cab1d538e118cebe29e0462dc05d
Sergio Costa <sergio.costa@outlook.com>
2014-10-24 12:13:23 -0300

    Added general variables, meta-robots
    changed some thing

--- header.php
+++ header.php
@@ -1,50 +1,83 @@
 <?php
 /**
  * @package Project Name
+ * @category Main theme files
+ * @author Sergio Costa
  */
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 9]>
-<script type="text/javascript">
-    window.location.href = "<?php bloginfo('siteurl'); ?>/ie/";
-</script>
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
-<html class="no-js" <?php language_attributes(); ?>>
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
+    <script>
+        var baseUrl = "<?php bloginfo('url'); ?>";
+    </script>
+    <!--[if lt IE 8]>
+       <script type="text/javascript"> window.location.href = baseUrl + "ie/index.html"; </script>
+    <![endif]-->
 
-<title><?php wp_title('|', true, 'right'); ?></title>
+    <?php
+        // General Variables
+        $title_default = get_bloginfo('name');
+        $keys_default  = '';
+        $link_default  = get_bloginfo('wpurl');
+        $desc_default  = get_bloginfo('description');
+        $image_default = get_bloginfo('template_url') . '/assets/img/logo.png';
+        if (is_single() || is_page()) {
+            $title_default = get_the_title($post->ID);
+            $link_default  = get_permalink();
+            if(has_post_thumbnail()){
+                $image_ID      = get_post_thumbnail_id(get_the_id());
+                $image_default = wp_get_attachment_image_src($image_ID, 'thumbnail');
+                $image_default = $image_default[0];
+            }
+        }
+    ?>
 
-<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
+    <title><?php wp_title('&raquo;', 'true', 'right'); ?></title>
 
-<meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="copyright" content="&copy; Copyright 2013 <?php bloginfo('name'); ?>" />
-<link type="text/plain" rel="author" href="<?php bloginfo('template_url'); ?>/humans.txt" />
-<meta name="keywords" content="<?php the_tags(); ?>" />
-<meta name="description" content="<?php bloginfo('description'); ?>" />
-<meta name="google-site-verification" content="" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link type="text/plain" rel="author" href="<?php bloginfo('template_url'); ?>/humans.txt" />
+    <meta name="copyright" content="&copy; Copyright <?php echo date('Y'); ?> <?php echo $title_default; ?>" />
+    <meta name="keywords" content="<?php echo $keys_default; ?>, <?php if($posttags){foreach($posttags as $tag){echo $tag->name . ', ';}}; ?>" />
+    <meta name="description" content="<?php echo $desc_default; ?>" />
+    <?php
+        if(is_single() || is_page() || is_category() || is_home()) {
+            echo '<meta name="robots" content="all,noodp" />';
+            echo "\n";
+        }
+        else if(is_archive()) {
+            echo '<meta name="robots" content="noarchive,noodp" />';
+            echo "\n";
+        }
+        else if(is_search() || is_404()) {
+            echo '<meta name="robots" content="noindex,noarchive" />';
+            echo "\n";
+        }
 
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-<link href="<?php bloginfo('template_url'); ?>/assets/images/icons/favicon.ico" rel="shortcut icon" />
-<!-- <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet"> -->
+        if (wp_is_mobile()) { ?>
+            <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/assets/img/icons/apple-touch-icon.png">
+            <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_url'); ?>/assets/img/icons/apple-touch-icon-72x72.png">
+            <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_url'); ?>/assets/img/icons/apple-touch-icon-114x114.png">
+            <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_url'); ?>/assets/img/icons/apple-touch-icon-144x144.png">
+    <?php } else { ?>
+            <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/assets/img/icons/favicon.ico" type="image/x-icon">
+    <?php } ?>
 
-<?php if (is_home()) { ?>
-    <meta property="og:title" content="<?php bloginfo('name'); ?>"/>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+    <meta property="og:title" content="<?php echo $title_default; ?>"/>
     <meta property="og:type" content="site"/>
-    <meta property="og:url" content="<?php bloginfo('siteurl'); ?>"/>
-    <meta property="og:image" content="<?php bloginfo('template_url'); ?>/screenshot.png"/>
-    <meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
-    <meta property="og:description" content="<?php bloginfo('description'); ?>"/>
-<?php } else { ?>
-    <meta property="og:title" content="<?php echo get_the_title($post->ID); ?>"/>
-    <meta property="og:type" content="site"/>
-    <meta property="og:url" content="<?php echo get_post_permalink($post->ID); ?>"/>
-    <meta property="og:image" content="<?php echo get_the_post_thumbnail($post->ID); ?>"/>
-    <meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
-    <meta property="og:description" content="<?php bloginfo('description'); ?>"/>
-<?php } ?>
+    <meta property="og:url" content="<?php echo $link_default; ?>"/>
+    <meta property="og:image" content="<?php echo $image_default; ?>"/>
+    <meta property="og:site_name" content="<?php echo $title_default; ?>"/>
+    <meta property="og:description" content="<?php echo $desc_default; ?>"/>
 
 <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+    <body <?php body_class(); ?>>
