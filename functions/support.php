00ed47f57aa8b7fad010a32679ae8896cc4698c8
zergiocosta <sergio.costa@outlook.com>
2015-06-06 00:01:09 -0300

    mobile menu functions added

--- functions/support.php
+++ functions/support.php
@@ -121,7 +121,68 @@ foreach ( $items as &$item ) {
 }
 return $items;
 }
-// swf
+
+/**
+ * Nav Menu Dropdown
+ *
+ * @package      BE_Genesis_Child
+ * @since        1.0.0
+ * @link         https://github.com/billerickson/BE-Genesis-Child
+ * @author       Bill Erickson <bill@billerickson.net>
+ * @copyright    Copyright (c) 2011, Bill Erickson
+ * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ *
+ */
+
+class Walker_Nav_Menu_Dropdown extends Walker_Nav_Menu {
+    function start_lvl(&$output, $depth){
+        $indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
+    }
+
+    function end_lvl(&$output, $depth){
+        $indent = str_repeat("\t", $depth); // don't output children closing tag
+    }
+
+    /**
+     * Start the element output.
+     *
+     * @param  string $output Passed by reference. Used to append additional content.
+     * @param  object $item   Menu item data object.
+     * @param  int $depth     Depth of menu item. May be used for padding.
+     * @param  array $args    Additional strings.
+     * @return void
+     */
+    function start_el(&$output, $item, $depth, $args) {
+        $url = '#' !== $item->url ? $item->url : '';
+        $classe = "item";
+        if ($depth == 0) {
+            $classe = "sub-item-mobile";
+        }
+        if ($classe == "sub-item-mobile") {
+            $output .= '<option class="'.$classe.'" value="' . $url . '">' . $item->title;
+        } else {
+            $output .= '<option class="'.$classe.'" value="' . $url . '">' . ' -- '.$item->title;
+        }
+    }
+
+    function end_el(&$output, $item, $depth){
+        $output .= "</option>\n"; // replace closing </li> with the option tag
+    }
+}
+
+/**
+ * Mobile Menu
+ *
+ */
+function be_mobile_menu() {
+    wp_nav_menu( array(
+        'walker'         => new Walker_Nav_Menu_Dropdown(),
+        'items_wrap'     => '<div class="mobile-menu text-center"><form><select class="form-control select-menu" onchange="if (this.value) window.location.href=this.value"><option value="javascript:void();" selected>Menu</option>%3$s</select></form></div>',
+    ) );
+}
+add_action( 'genesis_before_header', 'be_mobile_menu' );
+
+// allow swf files
 function add_upload_mime_types( $mimes ) {
     if ( function_exists( 'current_user_can' ) )
         $unfiltered = $user ? user_can( $user, 'unfiltered_html' ) : current_user_can( 'unfiltered_html' );
